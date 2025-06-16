d93779565e9e0fd68349bd71cc58c0672964a138
maar-ten <maar-ten@users.noreply.github.com>
2025-06-16 12:38:14 +0000

    use plane object

--- dump1090-logger.js
+++ dump1090-logger.js
@@ -4,12 +4,17 @@ const URL = 'http://172.17.0.1:8080/data.json';
 // const URL = 'http://localhost:8080/data.json';
 const POLLING_INTERVAL = 5 * 1000; // 5s
 const CACHE_EVICTION_TIME = 2 * 60 * 1000; // 2 minutes
-const LAT_HOME = Number(process.env.LAT_HOME) || 52;
-const LON_HOME = Number(process.env.LON_HOME) || 4;
+const LAT_HOME = Number(process.env.LAT_HOME) ?? 52;
+const LON_HOME = Number(process.env.LON_HOME) ?? 4;
+const TX = process.env.TZ ?? 'UTC';
 const R = 6371; // Radius of the earth in km
 
 console.log('Lat/lon of home are set to:', [LAT_HOME, LON_HOME]);
+console.log('Timezone set to:', TZ);
 console.log(`Fetching data from: ${URL} every ${POLLING_INTERVAL / 1000} sec.`);
+
+// Locale of Sweden (sv) looks similar to ISO
+const format = (datetime) => datetime.toLocaleString('sv', {timezone: TZ}).replace(' ', 'T');
 
 const planes = new Map();
 
@@ -17,9 +22,9 @@ const logger = fs.createWriteStream('./public/passing-planes.log', {
   flags: 'a' // append data to the file
 });
 
-const lowAltitudeLogger = fs.createWriteStream('./public/low-altitude-planes.log', {
-  flags: 'a' // append data to the file
-});
+// const lowAltitudeLogger = fs.createWriteStream('./public/low-altitude-planes.log', {
+//   flags: 'a' // append data to the file
+// });
 
 function logPlanesNearby() {
   // get and process plane data from dump1090
@@ -38,6 +43,7 @@ function logPlanesNearby() {
   // clean up of expired planes
   expiredPlanes.forEach(plane => planes.delete(plane.hex));
 }
+setInterval(logPlanesNearby, POLLING_INTERVAL);
 
 async function processResponse(response) {
   if (!response.ok) {
@@ -49,52 +55,18 @@ async function processResponse(response) {
 }
 
 function updatePlanes(plane) {
-  const currentDistance = getDistance(plane);
-  const cachedPlane = planes.get(plane.hex);
-  const now = Date.now();
-
-  if (cachedPlane && cachedPlane.closestDistance < currentDistance) {
-    // plane is further away so just update the last seen time
-    planes.set(plane.hex, {
-      ...cachedPlane,
-      measurements: ++cachedPlane.measurements,
-      lastSeen: now,
-    });
-    return;
+  if (planes.has(plane.hex)) {
+    planes.get(plane.hex).update(plane);
+  } else {
+    const newPlane = new Plane(plane);
+    planes.set(newPlane.hex, newPlane);
   }
-
-  // create a new set of closest parameters for this plane
-  planes.set(plane.hex, {
-    ...plane,
-    closestDistance: currentDistance,
-    closestDistanceTime: now,
-    closestDistanceLat: plane.lat,
-    closestDistanceLon: plane.lon,
-    closestDistanceAltitude: plane.altitude,
-    closestDistanceTrack: plane.track,
-    measurements: cachedPlane ? ++cachedPlane.measurements : 1,
-    lastSeen: now,
-  });
 }
 
 function logPlanes(planes) {
-  planes.forEach(plane => logger.write(`${toString(plane)}\n`));
-}
-
-function toString(plane) {
-  const timestamp = (epoch) => new Date(epoch).toISOString();
-  return [
-    timestamp(plane.lastSeen),
-    plane.hex,
-    plane.flight.trim(),
-    timestamp(plane.closestDistanceTime),
-    plane.closestDistanceLat,
-    plane.closestDistanceLon,
-    plane.closestDistance.toFixed(1),
-    plane.closestDistanceAltitude,
-    plane.closestDistanceTrack,
-    plane.measurements
-  ].toString();
+  planes.forEach(plane => {
+    logger.write(`${plane}\n`);
+});
 }
 
 function getDistance({lat, lon}) {
@@ -113,4 +85,63 @@ function toRad(angle) {
   return (angle * Math.PI) / 180;
 }
 
-setInterval(logPlanesNearby, POLLING_INTERVAL);
+class Plane {
+  hex;
+  plane;
+  path = [];
+  lastSeen;
+  closestDistancePlane;
+  closestDistance;
+  closestDistanceTime;
+
+  constructor(plane) {
+    const now = Date.now();
+
+    this.hex = plane.hex;
+    this.plane = plane;
+    this.path.push([plane.lat, plane.lon]);
+    this.closestDistance = getDistance(plane);
+    this.closestDistanceTime = now;
+    this.closestDistancePlane = plane;
+    this.lastSeen = now;
+  }
+
+  update(plane) {
+    const distance = getDistance(plane);
+    const now = Date.now();
+
+    if (distance < this.closestDistance) {
+      this.closestDistance = distance;
+      this.closestDistanceTime = now;
+      this.closestDistancePlane = plane;
+    }
+
+    this.lastSeen = now;
+
+    const coords = [plane.lat, plane.lon];
+    if (!this.equalLastCoords(coords)) {
+      this.path.push(coords);
+    }
+  }
+
+  equalLastCoords(coords) {
+    const lastCoords = this.path[this.path.length - 1];
+    return lastCoords[0] === coords[0] && lastCoords[1] === coords[1];
+  }
+
+  toString() {
+    return [
+      format(this.lastSeen),
+      this.hex,
+      this.closestDistancePlane.flight.trim(),
+      format(this.closestDistanceTime),
+      this.closestDistancePlane.lat,
+      this.closestDistancePlane.lon,
+      this.closestDistance.toFixed(1),
+      this.closestDistancePlane.altitude,
+      this.closestDistancePlane.speed,
+      this.closestDistancePlane.track,
+      this.path.length
+    ].toString();
+  }
+}
