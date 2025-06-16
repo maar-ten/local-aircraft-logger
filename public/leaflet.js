a2042a0ecc19392a1a5cca65a3e3b3409b1d64e6
maar-ten <maar-ten@users.noreply.github.com>
2025-06-16 19:00:36 +0000

    rename to aircrafts

--- public/leaflet.js
+++ public/leaflet.js
@@ -9,56 +9,56 @@ export class LeafletMap {
     }
 }
 
-export function addMarker(plane, map) {
-    const marker = L.marker([plane.lat, plane.lon], { icon: getIcon(plane.altitude, plane.track) }).addTo(map);
+export function addMarker(aircraft, map) {
+    const marker = L.marker([aircraft.lat, aircraft.lon], { icon: getIcon(aircraft.altitude, aircraft.track) }).addTo(map);
     marker.on('mouseover', () => marker.setZIndexOffset(10000));
     marker.on('mouseout', () => marker.setZIndexOffset(0));
-    marker.bindPopup(getPopupText(plane));
-    marker.setOpacity(plane.opacity ?? 1);
+    marker.bindPopup(getPopupText(aircraft));
+    marker.setOpacity(aircraft.opacity ?? 1);
     return marker;
 }
 
-export function addPath(plane, map) {
-    return L.polyline([[plane.lat, plane.lon]]).addTo(map);
+export function addPath(aircraft, map) {
+    return L.polyline([[aircraft.lat, aircraft.lon]]).addTo(map);
 }
 
 export function addPathPoints(points, map) {
     return L.polyline(points).addTo(map);
 }
 
-export function updateMarker(marker, plane) {
-    marker.setIcon(getIcon(plane.altitude, plane.track, true));
-    marker.setLatLng([plane.lat, plane.lon]);
+export function updateMarker(marker, aircraft) {
+    marker.setIcon(getIcon(aircraft.altitude, aircraft.track, true));
+    marker.setLatLng([aircraft.lat, aircraft.lon]);
     marker.setZIndexOffset(20000);
-    marker.setPopupContent(getPopupText(plane));
+    marker.setPopupContent(getPopupText(aircraft));
 }
 
-export function updatePath(path, plane) {
+export function updatePath(path, aircraft) {
     const coords = path.getLatLngs();
-    const newCoords = [plane.lat, plane.lon];
+    const newCoords = [aircraft.lat, aircraft.lon];
 
     if (coords[coords.length - 1] !== newCoords) {
-      path.addLatLng([plane.lat, plane.lon]);
+      path.addLatLng([aircraft.lat, aircraft.lon]);
     }
 }
 
 function getIcon(altitude, track, live = false) {
-    const rotation = -45 + Number(track); // -45 to correct the plane emoji's default rotation on most systems
-    const iconClass = live ? 'plane-icon-live' : 'plane-icon';
+    const rotation = -45 + Number(track); // -45 to correct the aircraft emoji's default rotation on most systems
+    const iconClass = live ? 'aircraft-icon-live' : 'aircraft-icon';
     const html = `<div class="${iconClass}" style="transform: rotate(${rotation}deg); background-color: ${getAltitudeColor(altitude)};">✈️</div>`;
     return L.divIcon({ html, className: '', iconSize: [30, 30] });
 }
 
-function getPopupText(plane) {
+function getPopupText(aircraft) {
     return ''.concat(
-        `hex: <a href="https://map.opensky-network.org/?icao=${plane.hex}">${plane.hex}</a>`,
-        `<br>flight: ${plane.flight}`,
-        `<br>time: ${plane.time}`,
-        `<br>distance: ${plane.distance} km`,
-        `<br>altitude: ${plane.altitude} ft`,
-        `<br>speed: ${plane.speed}`,
-        `<br>track: ${plane.track}`,
-        `<br>seen #: ${plane.measurements}`
+        `hex: <a href="https://map.opensky-network.org/?icao=${aircraft.hex}">${aircraft.hex}</a>`,
+        `<br>flight: ${aircraft.flight}`,
+        `<br>time: ${aircraft.time}`,
+        `<br>distance: ${aircraft.distance} km`,
+        `<br>altitude: ${aircraft.altitude} ft`,
+        `<br>speed: ${aircraft.speed}`,
+        `<br>track: ${aircraft.track}`,
+        `<br>seen #: ${aircraft.measurements}`
     );
 }
 
